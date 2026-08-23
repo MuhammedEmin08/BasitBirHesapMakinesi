9140b9f91e80a67b39920b11a6182774bac39289
Muhammed Emin Özsoy <m.eminozsoy9781@gmail.com>
2026-08-23 13:00:14 +0300

    BasitHesapMakinesi eklendi.

---
+++ main.py
@@ -0,0 +1,24 @@
+first_number = float(input("İlk Sayınızı giriniz : "))
+second_number = float(input("İkinci Sayınızı giriniz : "))
+which_procces  = input("toplama(+),çıkarma(-),çarpma(*),bölme(/) :")
+if which_procces == "+":
+    def toplama(x,y):
+        print(int(x + y))
+    toplama(first_number,second_number)
+elif which_procces == "-":
+    def çıkarma(x,y):
+        print(int(x - y))
+    çıkarma(first_number,second_number)
+elif which_procces == "*":
+    def çarpma(x,y):
+        print(int(x * y))
+    çarpma(first_number,second_number)
+elif which_procces == "/":
+    def bölme(x,y):
+        print(x / y)
+    bölme(first_number,second_number)
+else:
+    print("Lütfen belirtilmiş olan işlemleri seçin!")
+
+
+
